6a093ad28d67ca38aa988fdac40d0d3efdc6b561
laolyu <pytest@foxmail.com>
2021-09-09 17:27:01 +0800

    修改了杀updc的方式防止误杀弹窗进程

--- pop/pop_qq.py
+++ pop/pop_qq.py
@@ -153,13 +153,15 @@ def b4hand(project, package, updc, p_list):
 
         setTime[i]()
         logger.info('*****updc********')
-        p = subprocess.Popen(updc, shell=True)
         try:
-            p.communicate(timeout=20)
-        except subprocess.TimeoutExpired as e:
-            logger.info('timeout>kill process****', e)
-            subprocess.call(['taskkill', '/F', '/T', '/PID', str(p.pid)])
-        time.sleep(100)
+            subprocess.Popen(updc, shell=True)
+        except Exception as e:
+            logger.info(f'***kill {updc}****', e)
+        finally:
+            time.sleep(120)
+            result = subprocess.check_call(f'taskkill /F /IM {updc}.exe')
+            if result == 0:
+                logger.info(f'***{updc}.exe is killed*****')
 
         if project in [xiaoyu, kuaizip, kantu, heinote, finder, browser]:
             pb()
@@ -188,7 +190,7 @@ def b4hand(project, package, updc, p_list):
 
 if __name__ == '__main__':
     logger.add("gjl_log_{time}.log", rotation="500MB", encoding="utf-8", enqueue=True, compression="zip", retention="10 days")
-    projects = [xiaoyu, kuaizip, kantu, heinote, finder, browser, lszip, xinnote, qjpdf, cloudbar, haotu, xxbz, sesame, smartlook, xfpdf, jcbz]
+    projects = [xiaoyu, kuaizip, kantu, heinote, finder, browser, lszip, xinnote, qjpdf, cloudbar, haotu, xxbz, smartlook, xfpdf, jcbz, sesame]
     for i in range(len(projects)):
         project = projects[i]
         package = project['package']
