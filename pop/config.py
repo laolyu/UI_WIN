bae6e18fb13c87e8b639a220a96ee1713f25035d
laolyu <pytest@foxmail.com>
2021-09-14 10:09:17 +0800

    修复upgrade.exe.exe

--- pop/config.py
+++ pop/config.py
@@ -7,33 +7,33 @@ path_local = 'C:/Users/Administrator/AppData/Local/'
 
 xiaoyu = {
     'package': path_1 + 'xiaoyu_v3.3.0.7_lv_1.exe -wjm',
-    'updc': path_roaming + 'xiaoyu/upgrade.exe',
+    'updc': path_roaming + 'xiaoyu/upgrade',
     'p_list': ['melancholy', 'bqyptp', 'srzy2345setting', 'xymn', 'bqtp', 'bqpb', 'Anifacec', 'hh12345setting', 'bshjcbdsbh', 'yb32345setting', 'xytipsxhVV12',
                'xytipsxytt', 'mkiuhn', 'hsbdaefrdsf', 'spoiler', 'instrument', 'xytpopoth', 'qwaszx', 'sysssnew', 'Shawnmen', 'bgdcvn']
 }
 kuaizip = {
     'package': path_1 + 'KuaiZip_Setup_v3.3.0.7_lv_1.exe -wjm',
-    'updc': path_roaming + '快压/X86/KuaiZip.exe',
+    'updc': path_roaming + '快压/X86/KuaiZip',
     'p_list': ['qbylwl', 'restless', 'ghsauk', 'aghghf', 'Selenarctos', 'kuaiyatuop', 'wbdyl', 'kuaiyaminixktt', 'lakesi', 'hdagf', 'enthusiastic', 'hfhsbv',
                'gshuhg', 'alkanePanda', 'tghfsdg', 'vbvcxf', 'kwGOkLeLvH', 'gfswzahh', 'polarbear', 'kzyptp', 'Eg_NNIfiu', 'Kuaipb', 'torque', 'Jxohft',
                'kuaiyatpopxxrl', 'kuaiyatipsrytx', 'kuaiyatpopxktt', 'pineapple', 'YVyVcPZzs', 'VLscDaseKX']
 }
 kantu = {
     'package': path_1 + 'PhotoViewer_Setup_v3.3.0.6_lv_1 -wjm',
-    'updc': path_roaming + 'PhotoViewer/Update.exe',
+    'updc': path_roaming + 'PhotoViewer/Update',
     'p_list': ['Cuttings', 'armois', 'okquery', 'ABCminixktt', 'ABCtpopxktt', 'Xiralimy', 'ktpb', 'viewmnqtqd', 'wd22345setting', 'sj2345setting', 'lavenderhi',
                'sceuhfure', 'yy12345setting', 'Shawnmend', 'Danuvius', 'topether', 'tbadjswsr', 'ABCtpoprytx']
 }
 heinote = {
     'package': path_1 + 'Heinote_v3.3.0.2_lv_1.exe -wjm',
-    'updc': path_roaming + 'Heinote/upgrade.exe',
+    'updc': path_roaming + 'Heinote/upgrade',
     'p_list': ['xiaoheitipsrytx', '62345setting', 'siuywteinbg', 'mkiayhb', 'pbxhone', 'njaiah', 'Jsbyptp', 'qywtgj', 'xiaoheiminixhtt', 'heipan',
                'fddgfuwgfywjd', 'bhajiy', 'xiaoheitpopxhtt', 'ejtspfs2345setting', 'jrwpvfn2345setting', 'stone', 'fhreywfysa', 'dvwertbtf', 'xiaoheitipsxhtt',
                'nbxvchidi']
 }
 finder = {
     'package': path_1 + 'Finder_Setup_3.2.2.6_lv_1.exe -wjm',
-    'updc': path_local + 'finder/cc.exe',
+    'updc': path_local + 'finder/cc',
     'p_list': ['lWuXI', 'GStipsxktt', 'bdjsq', 'BaWrnG', 'DCsousuoss', 'gsminiya', 'GSscreensaver', 'gsurl', 'vvhTdX', 'gstpopgstt', 'GStpoprytx', 'srpanka',
                'IEKmKY', 'gsminikan', 'asajksa', 'SNss', 'ssfloattip', 'WtZTlg', 'Sisrwl', 'Bdnews', 'oaGIdb', 'gsminiqt', 'gsminigstt', 'GStipsrytx',
                'gsminixiao', 'RLnewsss', 'ssyptp', 'TEnMhP']
@@ -41,73 +41,73 @@ finder = {
 
 browser = {
     'package': path_1 + '7654Browser_v3.1.1.2_lv_1.exe -wjm',
-    'updc': path_roaming + '7654Browser/UpdateChecker.exe',
+    'updc': path_roaming + '7654Browser/UpdateChecker',
     'p_list': ['llqfloattip', '7654renwulan', 'thrill', '7654llqtips', 'changes', '7654weather', 'reunion', 'fracturesl', 'kk', 'borealis', '7654llqtuopan',
                'llqyptips', 'aiouniya', '7654pb', '7654llqurl']
 }
 
 lszip = {
     'package': path_1 + 'LsZip_Setup_v1.1.0.4_lv_1.exe -wjm',
-    'updc': path_roaming + '雷神压缩/ThorUpdateChecker.exe',
+    'updc': path_roaming + '雷神压缩/ThorUpdateChecker',
     'p_list': ['cSskSwZ', '12345ShellPro', 'Dyaqxzp', 'a2345NetFlow', 'Lstpopzip', 'Lstrayzip', 'HYbnhjkxs', 'yGdcOyvz', 'cbxOKVlhA', 'vYZjaDIRY', 'XxndaD',
                'yxJLQvVM', 'Lshenzip', 'Zzhwqfdfg', 'lshighzip']
 }
 
 jcbz = {
     'package': path_1 + 'JCWallpaper_v1.1.0.3_lv_1.exe -wjm',
-    'updc': path_roaming + '鲸彩壁纸/UpdateCheck.exe',
+    'updc': path_roaming + '鲸彩壁纸/UpdateCheck',
     'p_list': ['CWallpaper', 'cjblue', 'anthem', '22345NetFlow', 'yearn', 'Jcbztray', 'stalk', 'flexible', 'chord', 'Jcbzmini', 'Jcbztips', 'celery',
                'respond', 'expat', '12345Setting']
 }
 
 xinnote = {
     'package': path_1 + 'xinnote_v2.1.0.4_lv_1 -wjm',
-    'updc': path_roaming + 'xinnote/upgrade.exe',
+    'updc': path_roaming + 'xinnote/upgrade',
     'p_list': ['jskugi', 'xknllabwrk', 'bhagywi', 'xintray', 'cjblue', 'fsbtesadfg', 'xinnotegjsnsf', 'usfrbzq2345setting', 'djntguk2345setting', 'xinnotetips',
                'xinnotemini', 'dsdnfjrbef', 'vfplaw2345setting']
 
 }
 qjpdf = {
-    'package': path_1 + 'QingJiePdf_Setup_v1.1.0.3_lv_1.exe -wjm',
-    'updc': path_roaming + 'qingjiepdf/qjUpdateChecker.exe',
+    'package': path_1 + 'QingJiePdf_Setup_v1.0.1.5_lv_1.exe -wjm',
+    'updc': path_roaming + 'qingjiepdf/qjUpdateChecker',
     'p_list': ['crevice', 'update', 'llpokj', 'mBErTV', 'sxcde', 'mxqjPO', 'dqsPl', 'sWXluJ', 'ZFCEa', 'peimg', 'mxLK']
 }
 
 cloudbar = {
     'package': path_1 + 'clouds_setup_v1.1.0.2_lv_1.exe -wjm',
-    'updc': path_local + 'CloudsToolbar/updc.exe',
+    'updc': path_local + 'CloudsToolbar/updc',
     'p_list': ['Gjlground', 'chord', 'superb', 'stitch', 'humidity', 'episode', 'minor', 'updc']
 }
 
 xfpdf = {
     'package': path_1 + 'xfpdf_setup_v1.0.4.0_lv_1.exe -wjm',
-    'updc': path_roaming + 'WhirlwindPdf/Uc.exe',
+    'updc': path_roaming + 'WhirlwindPdf/Uc',
     'p_list': ['ghDSQgF', 'nbhgbd', 'xfminixftt', 'XFtnewsxftt', 'xfwJmu7Y', 'tvPPlZ', 'FGxWFah', 'LUxNQvY', 'oJiyZjS', 'WzsklOG', 'Gijmkxuf', 'mMNOcV',
                'xfminixftt''xftrayflash', 'xftpopxftt', 'xftipspdf']
 }
 
 smartlook = {
     'package': path_1 + 'preview_Setup_1.0.2.9_lv_1.exe -wjm',
-    'updc': path_roaming + 'SmartLook/preview.exe',
+    'updc': path_roaming + 'SmartLook/preview',
     'p_list': ['preview', 'ghierh', 'G2y6S', 'LznGsZ', 'personalcare', 'seasonalfresh', 'uDLGUe', 'NyVBT', 'rlwid', 'dailyfresh', 'dessertcookies', 'kDLGUe',
                'ZiQSGY', 'extider', 'AjFitD', 'rsgaBg', 'hardware', 'dailyfresh']
 }
 
 haotu = {
     'package': path_1 + 'haotu_v2.1.0.6_lv_1.exe -wjm',
-    'updc': path_roaming + 'haotukankan/Viewuc.exe',
+    'updc': path_roaming + 'haotukankan/Viewuc',
     'p_list': ['E240.TMP', 'zztpopkk', 'htkktp', 'xiaomitao', 'Weather', 'jgklfdgu', 'mqtyrp', 'baizhe', 'htkktpoprytx', 'xwView', 'Hyzus', 'htrwl', 'gajfgk',
                'amkfiopg', 'Update', 'Lolzzcxx']
 }
 
 xxbz = {
     'package': path_1 + 'CalfWallpaper_v1.0.3.5_lv_1.exe -wjm',
-    'updc': path_roaming + 'CalfWallpaper/UpdateCheck.exe',
+    'updc': path_roaming + 'CalfWallpaper/UpdateCheck',
     'p_list': ['Calftp', 'routine', 'beard', 'Pasture', 'Calendar', 'xxbztpopxx', 'participate', 'NVIDIRNM', 'bztp', 'sunjuly', 'CalfWallpaper']
 }
 
 sesame = {
     'package': path_1 + 'Sesame_Setup_v1.0.1.6_lv_1.exe -wjm',
-    'updc': path_roaming + '芝麻输入法/SesameUpgrade.exe',
+    'updc': path_roaming + '芝麻输入法/SesameUpgrade',
     'p_list': ['srfminiwwt', 'dessertcookies', 'srftipsWV23', 'srftpopwwm', 'hardware']
 }
