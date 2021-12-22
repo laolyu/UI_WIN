e37b20691ba83707a231a430d6ca94e9a0024ebc
lvmingyao <b4hand@qq.com>
2021-12-22 16:31:19 +0800

    360单线程运行

--- inst/inst_360.py
+++ inst/inst_360.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import datetime
-import random
 import subprocess
 import threading
 from lackey import *
 import sys
 from loguru import logger
 from time import sleep
+from random import sample
 
 Settings.InfoLogs = False
 sys.path.append(r'C:\zm\script\gjl')  # 先加入绝对路径，否则会报错，注意__file__表示的是当前执行文件的路径
@@ -59,20 +59,19 @@ def UI():
         logger.info(e)
 
 
-def cmd_send(project, path, vc_list):
-    logger.info('thread %s >>%s is running...' % (threading.current_thread().name, project))
+def cmd_send(path, vc_list):
+    logger.info('thread ->>%s is running...' % (threading.current_thread().name))
     now = datetime.datetime.now()
     s1 = now.strftime('%Y-%m-%d %H:%M:%S')
 
     for x in range(len(vc_list)):
         cmd = vc_list[x]
-        logger.info(f'{project}, {x + 1}, {cmd}')
-        m = random.randint(20, 60)
-        sleep(m)
+        logger.info(f' {x + 1}, {cmd}')
+        # sleep(30)
         for i in range(0, 3):
             p = subprocess.Popen(cmd, cwd=path, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             try:
-                out, err = p.communicate(timeout=90)
+                out, err = p.communicate(timeout=100)
                 err_info = err.decode('gbk')
                 logger.info(err_info)
             except subprocess.TimeoutExpired as e:
@@ -81,34 +80,28 @@ def cmd_send(project, path, vc_list):
                 continue
             break
 
-    logger.info('thread %s >>%s is ended...' % (threading.current_thread().name, project))
+    logger.info('thread >>%s is ended...' % (threading.current_thread().name))
     now = datetime.datetime.now()
     e1 = now.strftime('%Y-%m-%d %H:%M:%S')
-    logger.info(f"%s,start time: %s" % (project, s1))
-    logger.info("%s,end time: %s：" % (project, e1))
+    logger.info(f"start time: {s1}")
+    logger.info(f"end time: {e1}")
 
     start = datetime.datetime.strptime(s1, '%Y-%m-%d %H:%M:%S')
     end = datetime.datetime.strptime(e1, '%Y-%m-%d %H:%M:%S')
     total = end - start
     if (total.seconds) > 60:
         m = float(total.seconds) / 60
-        logger.info("%s,total(min)：%s" % (project, m))
+        logger.info("total(min)：%s" % m)
     else:
         m = total.seconds
-        logger.info("%s total(s)：%s" % (project, m))
+        logger.info("total(s)：%s" % m)
 
 
 if __name__ == '__main__':
     logger.add("gjl_log_{time}.log", rotation="500MB", encoding="utf-8", enqueue=True, compression="zip", retention="10 days")
     logger.info('thread %s is running...' % threading.current_thread().name)
-    path_0 = r'C:\zm\package\\'
+    path = r'C:\zm\package'
     UI()
-    version = version()
-    projects = list(version.keys())
-    for project in list(projects):
-        path = path_0 + project
-        vc_list = version[project]
-        t = threading.Thread(target=cmd_send, args=(project, path, vc_list), name='LoopThread')
-        t.start()
-        sleep(30)
+    v= sample(version(),len(version()))
+    cmd_send(path, v)
     logger.info('thread %s is ended...' % threading.current_thread().name)
